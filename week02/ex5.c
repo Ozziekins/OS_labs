21aad42c963a12c30e9fd545434a5b8bec4b7e97
Ozziekins <42877046+Ozziekins@users.noreply.github.com>
2019-08-28 15:05:16 +0300

    Update ex5.c

--- week02/ex5.c
+++ week02/ex5.c
@@ -1,5 +1,8 @@
+/* Modified version of exercise 3 */
+
 #include <stdio.h>
 
+/* function to print the full triangle */
 void full_tri(int *x){
 
 	for (int i = 1; i <= *x; ++i)
@@ -16,6 +19,7 @@ void full_tri(int *x){
 	}
 }
 
+/* function to print a half triangle */
 void half_tri(int *y){
 	for (int i = 1; i <= *y; ++i)
 	{
@@ -27,6 +31,7 @@ void half_tri(int *y){
 	}
 }
 
+/* function to print a rectangle */
 void rectangle(int *z){
 	for (int i = 0; i < *z; ++i)
 	{
@@ -43,6 +48,7 @@ int main(int argc, char const *argv[])
 	int n;
 	sscanf(argv[1], "%d", &n);
 
+	/* call the functions */
 	full_tri(&n);
 
 	printf("\n");

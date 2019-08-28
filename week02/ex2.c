eb803ee1e82cee54d6e1ed5fd6fa31aa8ed9e795
Ozziekins <42877046+Ozziekins@users.noreply.github.com>
2019-08-28 14:57:53 +0300

    Update ex2.c

--- week02/ex2.c
+++ week02/ex2.c
@@ -3,19 +3,23 @@
 
 int main()
 {
+	/* declare the array to contain the strings */
 	char c[555], r[555];
 	int count;
 
+	/* prompt fo ruser input */
 	printf("Please enter a string below:\n");
 	fgets(c, 555, stdin);
 	count = strlen(c) - 1;
 	
+	/* reverses the string and puts into another array */
 	for (int i = 0; i < strlen(c); ++i)
 	{
 		r[i] = c[count];
 		count--;
 	}
 
+	/* prints out the reversed string */
 	printf("The reversed string is: %s\n", r);
 
 	return 0;

1db37de3f28af2aa31c3f781be025553f1a98878
Ozziekins <42877046+Ozziekins@users.noreply.github.com>
2019-09-12 21:00:29 +0300

    Add files via upload
    An alternative approach for exercise 4

---
+++ week04/ex4_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+typedef char* String;
+
+int main(void)
+{
+	char command[555];
+	String exitCommand = "exit\n";
+	while(fgets(command, 100, stdin))
+	{
+		if (strcmp(command,exitCommand) == 0)
+		{
+			return 0;
+		}
+		system(command);
+	}
+	return 0;
+}
